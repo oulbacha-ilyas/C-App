f3c7644e5094ceff9eb78810d0abadceccc301ca
ilyasOL <I.Oulbacha2888@student.leedsbeckett.ac.uk>
2021-11-10 12:50:19 +0000

    implemented some drawing methods(line,rectangle) with fixed parametrs and can called by tyoing a command on the commandline

--- AseProgramingLanguage1/Form1.cs
+++ AseProgramingLanguage1/Form1.cs
@@ -30,5 +30,29 @@ namespace AseProgramingLanguage1
             Graphics g = e.Graphics;
             g.DrawImageUnscaled(OutputBitmap, 0, 0);
         }
+
+        private void commandLine_KeyDown(object sender, KeyEventArgs e)//ther user can input commands by specifying which graphics to draw with specific parametrs
+        {
+            
+            
+            if (e.KeyCode==Keys.Enter)
+            {
+                string command = commandLine.Text.Trim().ToLower();// reads what is written in command Line, gets ride of spaces and convert the text to lower case
+                if (command.Equals("line")==true)
+                {
+                    myCanvass.DrawLine(100, 100);
+                    //Console.WriteLine("a line is drawn");
+                }
+                else if (command.Equals("rectangle") == true)
+                {
+                    myCanvass.DrawRectangle(50, 100);
+                   // Console.WriteLine("a square is draws");
+                }
+                commandLine.Text = ""; // clear the command line after the ENTER KEY is pressed
+                outputWindow.Refresh();// update the drawing area to avoid drawing over past draws
+            }
+        }
+        
+
     }
 }
